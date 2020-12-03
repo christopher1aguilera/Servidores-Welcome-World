e1d60dcd474596254db747fe794062d220ba3d3a
christopheraguilera <christopher.aguilera.mad@gmail.com>
2020-12-03 16:45:53 -0300

    v.1

---
+++ script.js
@@ -0,0 +1,35 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+http
+.createServer(function (req, res) {
+const params = url.parse(req.url, true).query
+const nombre = params.nombre
+const nuevo = params.nuevo
+const contenido = params.contenido
+if (req.url.includes('/crear')) {
+fs.writeFile(nombre, contenido, () => {
+res.write('Archivo creado con éxito!')
+res.end()
+})
+}
+if (req.url.includes('/leer')) {
+fs.readFile(nombre, (err, data) => {
+res.write(data)
+res.end()
+})
+}
+if (req.url.includes('/renombrar')) {
+fs.rename(nombre, nuevo, (err, data) => {
+res.write(`${nombre} renombrado por ${nuevo}`)
+res.end()
+})
+}
+if (req.url.includes('/eliminar')) {
+fs.unlink(nombre, (err, data) => {
+res.write(`Archivo ${nombre} eliminado con éxito`)
+res.end()
+})
+}
+})
+.listen(8080, () => console.log('Escuchando el puerto 8080'))
